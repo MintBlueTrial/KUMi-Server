5453cb965ce1f7a54cf9dc26f4aa785e23dd27a3
DDDDanny <707956456@qq.com>
2021-08-18 16:51:51 +0800

    feat: 完成Demo，Get带参请求Demo

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(id: number): string {
+    if (id == 1) {
+      return 'Hello World!'
+    } else {
+      return 'Hello TypeScript! Hello Nest.js!';
+    }
   }
 }
