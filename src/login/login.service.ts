550f77b983efe1f85446e43922032851638e4eff
DDDDanny <707956456@qq.com>
2021-08-19 16:12:54 +0800

    fix: 调整用户登录数据格式

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class LoginService {
-    login(userInfo: object): object {;
+    login(userInfo: object): object {
         if (userInfo['name'] == 'admin' && userInfo['password'] == '123') {
-            return {'msg': '登录成功'}
+            return {...userInfo}
         } else {
             return {'msg': '登录失败'}
         }
