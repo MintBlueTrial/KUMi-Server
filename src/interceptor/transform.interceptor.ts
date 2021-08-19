1c9ec69cc069aec9ebc143d838b9ccf9d8c2ade4
DDDDanny <707956456@qq.com>
2021-08-19 16:11:59 +0800

    feat: 新增统一请求返回成功的数据

---
+++ src/interceptor/transform.interceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestInterceptor, CallHandler, ExecutionContext } from '@nestjs/common'
+import { map } from 'rxjs/operators'
+import { Observable } from 'rxjs'
+interface Response<T> {
+    data: T;
+}
+// 统一请求返回成功的数据
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(context: ExecutionContext,next: CallHandler<T>,): Observable<Response<T>> {
+    return next.handle().pipe(map(data => {
+        return {
+          data,
+          code: 0,
+          message: '请求成功',
+        };
+      }),
+    );
+  }
+}
