1f8449137bad6f7097d15f127f578b2dc6f97c06
DDDDanny <707956456@qq.com>
2021-08-19 16:12:30 +0800

    feat：全局注册拦截器（统一返回成功的数据）

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './interceptor/transform.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // 统一请求返回成功的数据
+  app.useGlobalInterceptors(new TransformInterceptor())
+  await app.listen(3000, () => {
+    Logger.log('✅ 服务已经正常启动');
+  });
 }
 bootstrap();
