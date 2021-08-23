be4177b4f9a608e6dfd7777b6cddd92316f27214
DDDDanny <707956456@qq.com>
2021-08-23 15:34:34 +0800

    feat: 新增响应体interface

---
+++ src/common/result.interface.ts
@@ -0,0 +1,12 @@
+/*
+* @Time    : 2021/08/23 15:26:50
+* @Author  : DannyDong
+* @File    : result.interface.ts
+* @Description: 请求响应体Interface
+*/
+
+export interface Response {
+    data?: any
+    msg?: string
+    code?: number
+}
