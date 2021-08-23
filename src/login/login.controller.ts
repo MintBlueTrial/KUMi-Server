13777a5d5a6f85d2528d9a9982d94b9e9265d012
DDDDanny <707956456@qq.com>
2021-08-23 15:36:45 +0800

    feat：调整控制器方法为异步方法

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,13 +1,21 @@
+/*
+* @Time    : 2021/08/23 15:28:20
+* @Author  : DannyDong
+* @File    : login.controller.ts
+* @Description: 登录控制器
+*/
+
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { User } from './login.schema';
+import { Response } from 'src/common/result.interface';
 
 @Controller('login')
 export class LoginController {
     constructor(private readonly loginService: LoginService) {}
 
     @Post()
-    login(@Body() userInfo: User): object{
-        return this.loginService.login(userInfo)
+    async login(@Body() userInfo: User): Promise<Response> {
+        return await this.loginService.login(userInfo)
     } 
 }
