934fbcdea87ef3a891f9ecd60711e73103ebb8d2
DDDDanny <707956456@qq.com>
2021-08-23 16:25:09 +0800

    style：调整代码缩进

--- src/login/login.schema.ts
+++ src/login/login.schema.ts
@@ -12,11 +12,11 @@ export type UserDocument = User & Document;
 
 @Schema()
 export class User extends Document {
-  @Prop({ required: true })
-  userName: string;
+    @Prop({ required: true })
+    userName: string;
 
-  @Prop({ required: true })
-  password: string;
+    @Prop({ required: true })
+    password: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User)
