1cd521571c79c71868ed6bdb42fd8285f4f86c89
DDDDanny <707956456@qq.com>
2021-09-05 22:03:24 +0800

    feat: 新增注册任务相关的逻辑&控制器

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,15 +9,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LoginModule } from './Login/login.module';
 import { UserModule } from './User/user.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
     // MongoDB数据库连接
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/KUMi', { useCreateIndex: true }),
     LoginModule,
-    UserModule
+    UserModule,
+    TaskModule
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
