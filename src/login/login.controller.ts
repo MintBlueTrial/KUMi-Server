fc58f30bffeb9520909c0218b953abdcbfcaa1ba
DDDDanny <707956456@qq.com>
2021-09-07 23:51:31 +0800

    feat：新增用户登录成功后，设置Cookie

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -5,17 +5,24 @@
 * @Description: 登录控制器
 */
 
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post, Res } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { User } from '../Schema/user.schema';
 import { Response } from 'src/Common/result.interface';
+import { Response as res } from 'express';
 
 @Controller('login')
 export class LoginController {
     constructor(private readonly loginService: LoginService) {}
 
     @Post()
-    async login(@Body() userInfo: User): Promise<Response> {
-        return await this.loginService.login(userInfo)
+    async login(@Body() userInfo: User, @Res({ passthrough: true }) response: res): Promise<Response> {
+        const result: Response = await this.loginService.login(userInfo)
+        if (result.code == 0) {
+            // 登录成功后，设置cookie
+            response.cookie('userId', result.data.userId, { httpOnly: true })
+            Logger.debug('Cookie设置成功')
+        }
+        return result
     } 
 }
