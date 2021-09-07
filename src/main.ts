fa22fd2d71b18f5bb577515c8fd0c04b1036c355
DDDDanny <707956456@qq.com>
2021-09-07 23:50:15 +0800

    feat：引入全局中间件，用于设置获取Cookie

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   await app.listen(3000, () => {
     Logger.log('✅ 服务已经正常启动');
   });
+  // 引入全局中间件（Cookie）
+  app.use(cookieParser());
 }
 bootstrap();
